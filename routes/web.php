35ab9ca4a46320ac9cd13974cc5f6d0254e87f21
rayannayar73 <rayannayar73@gmail.com>
2022-07-12 10:32:58 +0300

    first

--- routes/web.php
+++ routes/web.php
@@ -2,17 +2,38 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index_accueil');
+})->name('accueil_perso');
+
+Route::get('sign-in', function () {
+    return view('auth.connexion');
+})->name('sign-in');
+
+Route::get('create-compte', function () {
+    return view('create_compte.create_compte');
+})->name('create-compte');
+
+Route::get('/info_legale', function () {
+    return view('/info_legale');
 });
+Route::get('contact', function () {
+    return view('contact');
+});
+Route::get('contacts', function () {
+    return view('contacts');
+});
+
+Route::get('/politique_confidentialite', function () {
+    return view('/politique_confidentialite');
+})->name('politique_confidentialite');
+
+Route::get('/politique_confidentialites', function () {
+    return view('/politique_confidentialites');
+});
+Route::get('/tarifs', function () {
+    return view('/tarif');
+});
+
+Route::get('condition_generale_de_vente', 'ConditionController@index')->name('condition_generale_de_vente');
